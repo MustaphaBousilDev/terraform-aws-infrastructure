127cc8accbf0fdb10cd229e74423f96b90f0038c
Mustapha Bousil <bousilmustapha@gmail.com>
2025-08-12 02:22:23 +0100

    add configuration networking

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -19,6 +19,16 @@ provider "aws" {
   }
 }
 
+# Use networking module
+module "networking" {
+  source = "../../modules/networking"
+  
+  project_name       = var.project_name
+  environment        = var.environment
+  vpc_cidr          = "10.0.0.0/16"
+  availability_zones = ["us-east-1a", "us-east-1b"]
+}
+
 # Test S3 bucket
 resource "aws_s3_bucket" "test" {
   bucket = "${var.project_name}-${var.environment}-test"
