4d5e18d783284fb257f2ded74b94083937b72bcf
Mustapha Bousil <bousilmustapha@gmail.com>
2025-08-12 23:38:35 +0100

    start database config variable

---
+++ modules/database/variables.tf
@@ -0,0 +1,48 @@
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "ID of the VPC"
+  type        = string
+}
+
+variable "private_subnet_ids" {
+  description = "IDs of private subnets for database"
+  type        = list(string)
+}
+
+variable "app_security_group_id" {
+  description = "Security group ID of application servers"
+  type        = string
+}
+
+variable "db_instance_class" {
+  description = "RDS instance class"
+  type        = string
+  default     = "db.t3.micro"
+}
+
+variable "db_name" {
+  description = "Name of the database"
+  type        = string
+  default     = "appdb"
+}
+
+variable "db_username" {
+  description = "Database master username"
+  type        = string
+  default     = "admin"
+}
+
+variable "db_password" {
+  description = "Database master password"
+  type        = string
+  sensitive   = true
+}
