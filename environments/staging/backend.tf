761c22a28a00ca5cdcb2fdfb98bdb973c400f850
Mustapha Bousil <bousilmustapha@gmail.com>
2025-08-16 03:50:48 +0100

    err

--- environments/staging/backend.tf
+++ environments/staging/backend.tf
@@ -1,9 +1,11 @@
 terraform {
   backend "s3" {
-    bucket         = "terraformawsinfrastructure-terraform-state-7a6gvevg"
-    key            = "environments/staging-v2/terraform.tfstate" # ← Add -v2
-    region         = "us-east-1"
-    dynamodb_table = "terraformawsinfrastructure-terraform-locks"
-    encrypt        = true
+    bucket     = "terraformawsinfrastructure-terraform-state-7a6gvevg"
+    key        = "environments/staging-v2/terraform.tfstate"
+    region     = "us-east-1"
+    encrypt    = true
+    
+    # New way to enable locking
+    dynamodb_table = "terraformawsinfrastructure-terraform-locks"  # Still works for now
   }
 }
