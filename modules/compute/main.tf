cbe2e17f005df6a1c1d296281d7e2d3e1aad083f
Mustapha Bousil <bousilmustapha@gmail.com>
2025-08-19 19:26:22 +0100

    configure cloudwatch for alarm scale up

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -182,3 +182,29 @@ resource "aws_autoscaling_policy" "scale_down" {
   cooldown = 300 
   autoscaling_group_name = aws_autoscaling_group.app.name 
 }
+
+# Add CPU Scale-Up Alarm
+#--> Cloud WAtch Alarm - Scale UP when CPU is high
+resource "aws_cloudwatch_metric_alarm" "cpu_scale_up" {
+  alarm_name = "${var.project_name}-${var.environment}-cpu-scale-up"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods = 2 # 2 periods = 10 minutes
+  metric_name = "CPUUtilisation"
+  namespace = "AWS/EC2"
+  period = 300 # 5-minute periods
+  statistic = "Average"
+  threshold = 70 # 70% CPU threshold
+  alarm_description = "Scale up when CPU > 70% for 10 minutes"
+  # THIS IS THE KEY: Trigger the scale-up policy
+  alarm_actions = [aws_autoscaling_policy.scale_up.arn]
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.app.name
+  }
+  tags = {
+    Name = "${var.project_name}-${var.environment}-cpu-scale-up-alarm"
+    Purpose = "auto-scaling-trigger"
+  }
+}
+
+
+
