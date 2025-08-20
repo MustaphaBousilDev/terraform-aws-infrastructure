e449b61cc56271c9d7ffc57ce55a2c1a58950879
Mustapha Bousil <bousilmustapha@gmail.com>
2025-08-20 16:10:30 +0100

    add resource cloudfront with s3 bucket policy for this cloudfront access get

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -95,3 +95,72 @@ resource "aws_s3_bucket_lifecycle_configuration" "logs" {
     }
   }
 }
+
+
+
+# Origin Access Identity for S3
+resource "aws_cloudfront_origin_access_identity" "static_assets" {
+  comment = "CloudFront access to S3 static assets"
+}
+resource "aws_cloudfront_distribution" "static_assets" {
+  origin {
+    domain_name = aws_s3_bucket.static_assets.bucket_regional_domain_name
+    origin_id = "S3-${aws_s3_bucket.static_assets.id}"
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.static_assets.cloudfront_access_identity_path
+    }
+  }
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+  default_cache_behavior {
+    allowed_methods        = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "S3-${aws_s3_bucket.static_assets.id}"
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl     = 0
+    default_ttl = 3600
+    max_ttl     = 86400
+  }
+  price_class = "PriceClass_100"  # US, Canada, Europe
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-cdn"
+  }
+}
+
+resource "aws_s3_bucket_policy" "static_assets" {
+  bucket = aws_s3_bucket.static_assets.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "AllowCloudFrontAccess"
+        Effect    = "Allow"
+        Principal = {
+          AWS = aws_cloudfront_origin_access_identity.static_assets.iam_arn
+        }
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.static_assets.arn}/*"
+      }
+    ]
+  })
+}
