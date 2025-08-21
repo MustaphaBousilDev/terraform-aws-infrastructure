1dd446d8bf69bdcc578ed869100af19c8e75a670
Mustapha Bousil <bousilmustapha@gmail.com>
2025-08-21 15:49:52 +0100

    add security group for redis and made connection to redis by id security group from other ec2 that has this id

--- modules/cache/main.tf
+++ modules/cache/main.tf
@@ -8,3 +8,27 @@ resource "aws_elasticache_subnet_group" "redis" {
     }
 }
 
+#Security Group for redis 
+resource "aws_security_group" "redis" {
+    name_prefix = "${var.project_name}-${var.environment}-redis-"
+    vpc_id = var.vpc_id
+
+    ingress {
+        from_port       = 6379
+        to_port         = 6379
+        protocol        = "tcp"
+        //only allows connection to redis from servers that have this application security_group attached
+        security_groups = [var.app_security_group_id]
+    }
+
+    egress {
+        //from all port
+        from_port   = 0
+        //to all port 
+        to_port     = 0
+        // protocol=-1 (All Protocol)(TCP, UDP, ICMP)
+        protocol    = "-1"
+        //Anywhere on the internet
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
