58fa9dc3af1e741a3fc8914cef1ab11ca3933d93
Mustapha Bousil <bousilmustapha@gmail.com>
2025-08-21 17:16:42 +0100

    aws cloudwatach alarm for redis high memory 85%

--- modules/monitoring/redis_alarms.tf
+++ modules/monitoring/redis_alarms.tf
@@ -17,3 +17,23 @@ resource "aws_cloudwatch_metric_alarm" "redis_high_cpu" {
     CacheClusterId = var.redis_cluster_id
   }
 }
+
+# Redis Memory Usage Alarm
+resource "aws_cloudwatch_metric_alarm" "redis_high_memory" {
+  count = var.enable_redis_monitoring ? 1 : 0
+  
+  alarm_name          = "${var.project_name}-${var.environment}-redis-high-memory"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "DatabaseMemoryUsagePercentage"
+  namespace           = "AWS/ElastiCache"
+  period              = 300
+  statistic           = "Average"
+  threshold           = 85
+  alarm_description   = "Redis memory usage is high"
+  alarm_actions       = var.enable_sns_notifications ? [aws_sns_topic.alerts[0].arn] : []
+  
+  dimensions = {
+    CacheClusterId = var.redis_cluster_id
+  }
+}
