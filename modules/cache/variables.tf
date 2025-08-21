94bde942a656c2dc6e116f6499feccb3bad69f2a
Mustapha Bousil <bousilmustapha@gmail.com>
2025-08-21 15:39:11 +0100

    starting module caching

---
+++ modules/cache/variables.tf
@@ -0,0 +1,30 @@
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "private_subnet_ids" {
+  description = "Private subnet IDs"
+  type        = list(string)
+}
+
+variable "app_security_group_id" {
+  description = "Application security group ID"
+  type        = string
+}
+
+variable "redis_node_type" {
+  description = "ElastiCache node type"
+  type        = string
+  default     = "cache.t3.micro"
+}
