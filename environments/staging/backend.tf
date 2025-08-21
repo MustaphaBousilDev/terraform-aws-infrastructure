47f548b82bb73ac8dd978988b11c6ac559d7b860
Mustapha Bousil <bousilmustapha@gmail.com>
2025-08-21 17:41:35 +0100

    er

--- environments/staging/backend.tf
+++ environments/staging/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket  = "terraformawsinfrastructure-terraform-state-7a6gvevg"
     key     = "environments/staging-v2/terraform.tfstate"
-    region  = "us-east-1"
+    region  = "us-east-1" 
     encrypt = true
 
     # New way to enable locking
