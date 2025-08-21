bc766ffcae59326c6a9bad9173f85e7e5c0aa94a
Mustapha Bousil <bousilmustapha@gmail.com>
2025-08-21 15:53:55 +0100

    add cluster elasticache replication group

--- modules/cache/main.tf
+++ modules/cache/main.tf
@@ -31,4 +31,30 @@ resource "aws_security_group" "redis" {
         //Anywhere on the internet
         cidr_blocks = ["0.0.0.0/0"]
     }
+    tags = {
+        Name = "${var.project_name}-${var.environment}-redis-sg"
+    }
+}
+
+#ElasticCahe Redis Cluster 
+resource "aws_elasticache_replication_group" "redis" {
+    replication_group_id       = "${var.project_name}-${var.environment}-redis"
+    description                = "Redis cluster for ${var.project_name} ${var.environment}"
+    node_type                  = var.redis_node_type
+    port                       = 6379
+    parameter_group_name       = "default.redis7"
+    num_cache_clusters         = 2
+
+    automatic_failover_enabled = true
+    multi_az_enabled          = true
+
+    subnet_group_name          = aws_elasticache_subnet_group.redis.name
+    security_group_ids         = [aws_security_group.redis.id]
+
+    at_rest_encryption_enabled = true
+    transit_encryption_enabled = true
+
+    tags = {
+        Name = "${var.project_name}-${var.environment}-redis"
+    }
 }
