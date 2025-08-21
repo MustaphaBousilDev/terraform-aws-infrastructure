94810961c66cdd0a807bf5fd1b31a4237ab7f6fd
Mustapha Bousil <bousilmustapha@gmail.com>
2025-08-21 12:51:45 +0100

    add monitoring alarm for cloudfront low cache hit rate

--- modules/monitoring/cloudfront_alarm.tf
+++ modules/monitoring/cloudfront_alarm.tf
@@ -16,3 +16,20 @@ resource "aws_cloudwatch_metric_alarm" "cloudfront_high_error_rate" {
     }
 }
 
+resource "aws_cloudwatch_metric_alarm" "cloudfront_low_cache_hit_rate" {
+    count = var.enable_cloudfront_monitoring ? 1 : 0
+
+    alarm_name          = "${var.project_name}-${var.environment}-cloudfront-low-cache-hit-rate"
+    comparison_operator = "LessThanThreshold"
+    evaluation_periods  = 3
+    metric_name         = "CacheHitRate"
+    namespace           = "AWS/CloudFront"
+    period              = 300
+    statistic           = "Average"
+    threshold           = 70  # 70% cache hit rate
+    alarm_description   = "CloudFront cache hit rate is low"
+    alarm_actions       = var.enable_sns_notifications ? [aws_sns_topic.alerts[0].arn] : []
+    dimensions = {
+        DistributionId = var.cloudfront_distribution_id
+    }
+}
