aa48171580758b2c4c41a944ff83176be4b3466c
MustaphaBousilDev <bousilmustapha@gmail.com>
2025-08-27 21:40:35 +0100

    add S3 Endpoint Gateway

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -163,3 +163,17 @@ resource "aws_iam_role_policy" "flow_log" {
     ]
   })
 }
+
+#S3 Gateway Endpoint (its free)
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id = aws_vpc.main.id 
+  service_name = "com.amazonaws.${data.awservice_region.current.name}.s3"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids = concat(
+    [aws_route_table.public.id],
+    aws_route_table.private[*].id
+  )
+  tags = {
+    Name = "${var.project_name}-${var.environment}-s3-endpoint"
+  }
+}
