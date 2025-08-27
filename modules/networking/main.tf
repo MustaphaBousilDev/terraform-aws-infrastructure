6532f13bc96e26b17a8d886250938a6eedfc9b8a
MustaphaBousilDev <bousilmustapha@gmail.com>
2025-08-27 21:42:44 +0100

    add dynamodb resource Gateway endpoint

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -177,3 +177,19 @@ resource "aws_vpc_endpoint" "s3" {
     Name = "${var.project_name}-${var.environment}-s3-endpoint"
   }
 }
+
+# DynamoDB Gateway Endpoint (Free)
+resource "aws_vpc_endpoint" "dynamodb" {
+  vpc_id       = aws_vpc.main.id
+  service_name = "com.amazonaws.${data.aws_region.current.name}.dynamodb"
+  
+  vpc_endpoint_type = "Gateway"
+  route_table_ids = concat(
+    [aws_route_table.public.id],
+    aws_route_table.private[*].id
+  )
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-dynamodb-endpoint"
+  }
+}
