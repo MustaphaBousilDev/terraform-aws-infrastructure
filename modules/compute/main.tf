403795e09f926cb7ea0ec5e86afc2e8b48e83228
MustaphaBousilDev <bousilmustapha@gmail.com>
2025-08-30 22:50:58 +0100

    add redirectyion from http to https

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -267,8 +267,6 @@ resource "aws_acm_certificate" "main" {
   }
 }
 
-
-# HTTPS Listener for ALB
 # HTTPS Listener for ALB
 resource "aws_lb_listener" "https" {
   count = var.enable_ssl ? 1 : 0
@@ -286,5 +284,29 @@ resource "aws_lb_listener" "https" {
 
   tags = {
     Name = "${var.project_name}-${var.environment}-https-listener"
+  }
+}
+
+
+# HTTP to HTTPS Redirect
+resource "aws_lb_listener" "http_redirect" {
+  count = var.enable_ssl ? 1 : 0
+
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-http-redirect"
   }
 }
